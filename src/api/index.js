1233bbf1732251f7aca6c6436cd04a0d163f3102
Kashish Jain <jainabhishek7204@gmail.com>
2021-03-14 18:24:12 +0530

    Update backend proxy

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 //Local
-const appURL = "http://localhost:8080/messages";
-const authURL = "http://localhost:8080/auth";
+// const appURL = "http://localhost:8080/messages";
+// const authURL = "http://localhost:8080/auth";
 
 // Hosted
-// const appURL = "https://whatsapp04-backend.herokuapp.com/messages";
-// const authURL = "https://whatsapp04-backend.herokuapp.com/auth";
+const appURL = "https://whatsapp04-backend.herokuapp.com/messages";
+const authURL = "https://whatsapp04-backend.herokuapp.com/auth";
 
 export const appInstance = axios.create({ baseURL: appURL });
 export const authInstance = axios.create({ baseURL: authURL });
